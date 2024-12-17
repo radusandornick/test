f25d6f988558958d4249051f6ac551a5a65ebeb5
radusandornick <radusandor@yahoo.com>
2024-12-17 05:33:03 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# test
+# Setup
+
+Make sure you install the dependencies.
+```bash
+npm install
+```
+
+Add your environment variables in the .env.local file or .env. Example included in .env.example
+
+For detailed setup, watch the video here:
+https://youtu.be/DNtsJlmPda8
+https://youtu.be/7h1Dp9mbf7Q
---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import prisma from "./lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "./auth.config";
+
+const publicRoutes = ["/auth/signin", "/auth/signup"];
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt",
+    },
+    ...authConfig
+})
